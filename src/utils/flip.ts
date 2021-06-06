25908cc7f58d3faa40b399486813e3a91d601e73
Chia Wei <ongchiawei@gmail.com>
2021-06-06 17:32:38 +0200

    patch flip to check for dimension equality before animating

--- src/utils/flip.ts
+++ src/utils/flip.ts
@@ -5,6 +5,10 @@ export async function flip(elm: HTMLElement, cb: CallableFunction) {
 
   const rect2 = elm.getBoundingClientRect();
 
+  if (rectEquals(rect1, rect2)) {
+    return;
+  }
+
   const dX = rect1.left - rect2.left;
   const dY = rect1.top - rect2.top;
   const dW = rect1.width / rect2.width;
@@ -28,3 +32,16 @@ export async function flip(elm: HTMLElement, cb: CallableFunction) {
     },
   );
 }
+
+const rectEquals = (rect1: DOMRect, rect2: DOMRect) => {
+  return (
+    rect1.top === rect2.top &&
+    rect1.left === rect2.left &&
+    rect1.bottom === rect2.bottom &&
+    rect1.right === rect2.right &&
+    rect1.width === rect2.width &&
+    rect1.height === rect2.height &&
+    rect1.x === rect2.x &&
+    rect1.y === rect2.y
+  );
+};
